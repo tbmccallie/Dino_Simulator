248df86d469dea817afc6520fea8961df3832a9c
tbmccallie <67876536+tbmccallie@users.noreply.github.com>
2020-07-23 22:34:51 -0500

    Updated image.load()
    Added .convert_alpha() to image.load function in Dino initialization code to convert image to the same pixel format as the screen to ensure optimum performance.

--- dino_simulator.py
+++ dino_simulator.py
@@ -15,7 +15,7 @@ class Dino(pygame.sprite.Sprite):
     if it hits a wall"""
     def __init__(self, image_file, speed, location):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load(image_file)
+        self.image = pygame.image.load(image_file).convert_alpha()
         self.rect = self.image.get_rect()
         self.speed = speed
         self.rect.left, self.rect.top = location
