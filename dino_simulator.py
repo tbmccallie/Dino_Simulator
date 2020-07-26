7c86269f95bd586f26afb773784fb110e2026848
tbmccallie <67876536+tbmccallie@users.noreply.github.com>
2020-07-26 00:06:18 -0500

    Updated move function
    Updated move function to include more randomized sprite movement (instead of the sprite just bouncing around the enclosure in straight lines). Included code to flip the sprite if the x-value of the speed variable changes from positive to negative, or vice versa. Added to the if statements that flip the sprite if it hits the enclosure walls so that the sprite doesn't get stuck due to flipping from the previous if statements.

--- dino_simulator.py
+++ dino_simulator.py
@@ -4,7 +4,7 @@
 
 
 try:
-    import pygame, sys
+    import pygame, sys, random
 except ImportError:
     print("Couldn't load module")
     sys.exit()
@@ -20,13 +20,29 @@ class Dino(pygame.sprite.Sprite):
         self.speed = speed
         self.rect.left, self.rect.top = location
     def move(self):
-        """ Moves dino sprite, reverses speed if it hits enclosure \
+        """ Moves dino sprite, randomizes speed every 40 steps, reverses speed if it hits enclosure \
         walls, reverses image if it hits left or right wall"""
+        if step == 40 and self.speed[0] not in range(-5, 0): # Randomizes movement after 40 steps and flips sprite (if x-value of speed variable changes from positive to negative
+            self.speed[0] = random.randint(-5, -1)
+            self.speed[1] = random.randint(-7, 7)
+            self.image = pygame.transform.flip(self.image, 1, 0)
+        elif step == 40 and self.speed[0] in range(-5, 0): # Randomizes movement after 40 steps, but doesn't flip sprite because x-value of speed variable doesn't change from positive to negative
+            self.speed[0] = random.randint(-5, -1)
+            self.speed[1] = random.randint(-7, 7)
+        if step >= 80 and self.speed[0] not in range(1, 6): # # Randomizes movement after 80 steps and flips sprite (if x-value of speed variable changes from negative to positive)
+            self.speed[0] = random.randint(1, 5)
+            self.speed[1] = random.randint(-7, 7)
+            self.image = pygame.transform.flip(self.image, 1, 0)
+        elif step >= 80 and self.speed[0] in range(1, 6): # Randomizes movement after 80 steps, but doesn't flip sprite because x-value of speed variable doesn't change from negative to positive
+            self.speed[0] = random.randint(1, 5)
+            self.speed[1] = random.randint(-7, 7)
         if self.rect.left > 627 or self.rect.left < 173:
-            self.speed[0] = - self.speed[0]
-            self.image = pygame.transform.flip(self.image, 1, 0) # Turns the dino when it hits the left or right side of the enclosure
+            if step != 40 and step != 80: # Keeps sprite from getting stuck on the wall in an endless cycle of flipping
+                self.speed[0] = - self.speed[0]
+                self.image = pygame.transform.flip(self.image, 1, 0) # Turns the dino when it hits the left or right side of the enclosure
         if self.rect.top > 385 or self.rect.top < 15:
-            self.speed[1] = - self.speed[1]
+            if step != 40 and step != 80:
+                self.speed[1] = - self.speed[1]
         newpos = self.rect.move(self.speed)
         self.rect = newpos
 
@@ -37,6 +53,8 @@ def main():
     screen.fill([255, 255, 255])
     clock = pygame.time.Clock()
     my_dino = Dino("dino_model.png", [5, 7], [400, 130])
+    global step
+    step = 0
 
 
     background = pygame.Surface(screen.get_size())
@@ -69,6 +87,9 @@ def main():
         pygame.time.delay(300)
         screen.blit(background, (0, 0))
         my_dino.move()
+        step += 1
+        if step > 80:
+            step = 0
         screen.blit(my_dino.image, my_dino.rect) # This, combined with the 2 lines above it, draw the dinosaur in its new position and erases it from its previous position
         pygame.display.flip()
     pygame.quit()
